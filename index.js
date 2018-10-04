60d03b52d54ad0fef174d378087c32695259199d
novemberde@nate.com <novemberde@nate.com>
2018-10-04 09:44:42 +0900

    Apply options

--- index.js
+++ index.js
@@ -2,40 +2,45 @@
 'use strict';
 
 const { lstatSync, readdirSync } = require('fs')
-const { join } = require('path')
+const { join  } = require('path')
 const simpleGit = require('simple-git')
-const path = require('path')
+const program = require('commander');
+
+const packageJson = require('./package.json');
+
+program
+  .version(packageJson.version)
+  .option('-d, --directory [value]', 'Target directory')
+  .option('-r, --remote [value]', 'Git Remote', 'origin')
+  .option('-b, --branch [value]', 'Git branch', 'master')
+  .parse(process.argv);
+
+program.on('--help', function(){
+  console.log(`usage: git-puller -d <DIRECTORY_NAME>
+
+examples: 
+  git-puller -d .                                # Current directory
+  git-puller -d ../../_my_project                # Other directory
+  git-puller -d ./ -r origin -b master           # Specify remote and branch`);
+});
+
+const branch = program.branch;
+const remote = program.remote;
+const targetPath = program.directory;
+
+if(!targetPath) return program.help();
 
 const isDirectory = source => lstatSync(source).isDirectory()
 const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory)
-
-const [
-  targetPath,
-  remote="origin",
-  branch="master"
-] = process.argv.slice(2);
-
-if(!targetPath || targetPath === "--help") {
-  const helpText = `
-usage: git-puller <DIRECTORY_NAME>
-
-examples: 
-  git-puller .                                # current directory
-  git-puller ../../_my_project                # other directory
-  git-puller ../../_my_project origin master  # git pull REMOTE BRANCH`
-  console.log(helpText);
-  return;
-}
-
 const targetDirectories = getDirectories(targetPath).concat(targetPath);
 
 targetDirectories.map((targetDirectory) => {
   const git = simpleGit(targetDirectory);
-  
+
   git.fetch().pull(remote, branch, (err, update) => {
     if(err) return;
+    console.log('\nDirectory: ', join(process.cwd(), targetDirectory));
     console.log(update);
     console.log();
   });
-
 });
